6474607402480e19af2d5c61278fe0ca030b81cd
Yogesh Aswar <yogeshaswar46@gmail.com>
2023-01-24 13:18:06 +0530

    added emoji meaning

--- src/App.js
+++ src/App.js
@@ -5,26 +5,34 @@ import { useState } from 'react';
 
 function App() {
   const emojiDictionary = {
-    "😀": "Name: Grinning",
-    "😃": "Name: Smiley",
-    "😄": "Name: Smile",
-    "😁": "Name: Grin",
-    "😆": "Name: Laughing"
+    "😀": "Grinning",
+    "😃": "Smiley",
+    "😄": "Smile",
+    "😁": "Grin",
+    "😆": "Laughing"
+  };
+  const emojiMeaningDictionary = {
+    "😀": "Meaning: It conveys cheerfulness and joy towards something positive",
+    "😃": "Meaning: Smiley emoji denotes happiness and positive feelings",
+    "😄": "Meaning: The smile emoji depicts a person’s happiness when sending a greeting text or simply used to compliment something or the other ",
+    "😁": "Meaning: The emoji depicts laughter often used to react towards something hilarious"
   };
 
   var [ emojiName, setEmojiName ] = useState("");
-
+  var [ emojiMeaning, setEmojiMeaning ] = useState("");
   function onChangeHandler(event) {
     var emoji = event.target.value;
     emojiName = emojiDictionary[emoji];
     setEmojiName(emojiName);
+    setEmojiMeaning(emojiMeaningDictionary[emoji]);
   }
 
   return (
     <div className="App">
       <h1>Emoji Identifier</h1>
       <input type="text" onChange={onChangeHandler} placeholder='Enter your favorite emoji here'/>
-      <h4>{emojiName}</h4>
+      <h4>Name: {emojiName}</h4>
+      <p>{emojiMeaning}</p>
     </div>
   );
 }
